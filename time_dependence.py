3b6ca94dbeb99808b7517e4fe0544fdaba27354d
Zachary Boice <65305552+zboice@users.noreply.github.com>
2025-11-19 21:24:26 -0500

    addition 11/19
    Did more work on this, seems to be working. Next stop, wave equation?

--- time_dependence.py
+++ time_dependence.py
@@ -51,7 +51,7 @@ class Galerkin:
             self.nstart = 1
             self.bas = Galerkin.sines
             self.bas_der = Galerkin.sine_deriv
-            self.inho = lambda n, t: self.bas(n, a, self.coord) * inhomogeneous #* np.exp(-1j*t)
+            self.inho = lambda n, t: np.exp(-10j*t)*inhomogeneous
             self.operator = lambda n, m: (c0*self.bas(n, a, self.coord)*self.bas(m, a, self.coord) 
                                       + c1*self.bas(n, a, self.coord)*self.bas_der(m, a, self.coord) 
                                       + c2*self.bas_der(n, a, self.coord)*self.bas_der(m, a, self.coord)
@@ -74,54 +74,56 @@ class Galerkin:
         self.times = times
         self.basis_init(inhomogeneous, c0, c1, c2, c_non_lin)
 
-    def galerkin(self, t, h, a, N):
-        mat = np.zeros([N-self.nstart, N-self.nstart])
-        f_vec = np.zeros(N-self.nstart)
+    def derivs(self, t, c_vec, h, a, N):
+        M_mat = np.zeros([N-self.nstart, N-self.nstart])
+        L_mat = np.zeros_like(M_mat)
+        F_vec = np.zeros(N-self.nstart)
 
         for n in range(self.nstart, N):
             for m in range(self.nstart, N):
-                mat[n-self.nstart, m-self.nstart] = simpson_int(self.operator(n, m), h)
+                M_mat[n-self.nstart, m-self.nstart] = simpson_int(self.bas(n, a, Xs)*self.bas(m, a, Xs), h)
+                L_mat[n-self.nstart, m-self.nstart] = simpson_int(self.bas_der(n, a, Xs)*self.bas_der(m, a, Xs), h)
 
-            f_vec[n-self.nstart] = simpson_int( self.inho(n, t), h)
+            F_vec[n-self.nstart] = simpson_int( self.bas(n, a, self.coord)*self.inho(n, t) , h)
 
-        sol_vec = np.matmul(np.linalg.inv(mat), f_vec)
+        return np.matmul(np.linalg.inv(M_mat), (F_vec - np.matmul(L_mat, c_vec)))
 
-        answer = 0
-        for n in range(self.nstart, N):
-            answer += self.bas(n, a, self.coord)*sol_vec[n-self.nstart]
-
+    def initial_cvec(self, answer, c0):
+        for n in range(self.nstart, self.num):
+            answer[n-self.nstart] = 2/a * simpson_int(self.bas(n, a, self.coord)*c0, h)
         return answer
-    
-    def runge_kutta(self):
+
+    def runge_kutta(self, u0):
         int_time = range(len(self.times))
         w = 1
 
-        answer = np.zeros_like(self.coord)
-        rvecs = np.zeros([len(int_time), len(answer)], complex)
-        rvecs[0] = initial_rvec(self.coord)
+        cvecs = np.zeros([len(int_time), self.num-self.nstart])
+        cvecs[0] = self.initial_cvec(cvecs[0], u0)
 
         a = Xs[-1]
         h = Xs[1]-Xs[0]
         N = self.num
         ht = self.times[1]-self.times[0]
 
-        for j, t in enumerate(self.times[:-1]): # definitely need to do some tweaking here
-            # we need the basis for the n+1th time step to be informed by the nth time step?!
-            k1 = ht*self.galerkin(t, h, a, N)
-            k2 = ht*self.galerkin(t+0.5*ht, h, a, N)
-            k3 = ht*self.galerkin(t+0.5*ht, h, a, N)
-            k4 = ht*self.galerkin(t+ht, h, a, N)
+        for j, t in zip(range(len(self.times[:-1])), self.times[:-1]):
+            k1 = ht*self.derivs(t, cvecs[j], h, a, N)
+            k2 = ht*self.derivs(t+0.5*ht, cvecs[j]+0.5*k1, h, a, N)
+            k3 = ht*self.derivs(t+0.5*ht, cvecs[j]+0.5*k2, h, a, N)
+            k4 = ht*self.derivs(t+ht, cvecs[j]+k3, h, a, N)
 
-            '''
-            k1 = ht*self.galerkin(t, rvecs[j], w, answer, xs, hx, Nx, gal_num)
-            k2 = ht*self.galerkin(t+0.5*ht, rvecs[j]+0.5*k1, w, answer, xs, hx, Nx, gal_num)
-            k3 = ht*self.galerkin(t+0.5*ht, rvecs[j]+0.5*k2, w, answer, xs, hx, Nx, gal_num)
-            k4 = ht*self.galerkin(t+ht, rvecs[j]+k3, w, answer, xs, hx, Nx, gal_num)
-            '''
-
-            rvecs[j+1] = rvecs[j] + (k1+2*k2+2*k3+k4)/6
+            cvecs[j+1] = cvecs[j] + (k1+2*k2+2*k3+k4)/6
         
-        return rvecs, int_time
+        self.int_times = int_time
+        self.weights = cvecs
+    
+    def calc_answer(self):
+        answer = np.zeros([len(self.times), len(self.coord)])
+        for time in self.int_times:
+            sol_vec = self.weights[time]
+            for n in range(self.nstart, self.num):
+                answer[time] += self.bas(n, a, self.coord)*sol_vec[n-self.nstart]
+            
+        return answer, self.int_times
 
 def plotAnimated(Nx, x, t, func, ymin=-1, ymax=1):
     #plt.style.use('dark_background')
@@ -133,30 +135,37 @@ def plotAnimated(Nx, x, t, func, ymin=-1, ymax=1):
     plt.show()
 
 def init(line):
-        line.set_data([], [])
-        return line,
+    line.set_data([], [])
+    return line,
 
 def animate(t, line1, Nx, x, func):
-        line1.set_data(x, np.real(func[t][:2*Nx]))
-        return line1,
-
-def initial_rvec(Xs):
-    return np.zeros_like(Xs) #-(Xs-0.5*a*np.ones_like(Xs))**2 + (0.5*a*np.ones_like(Xs))**2
+    line1.set_data(x, np.real(func[t][:2*Nx]))
+    return line1,
 
 def inhomogeneous(Xs):
     return delta #np.cos(Xs) 
 
+def initial_func(Xs):
+    return Xs
+
 a = 1
 k = 3
-h = 0.5e-3
+h = 0.1e-2
 Xs = np.arange(0, a, h)
-times = np.linspace(0, 20, 100)
+times = np.arange(0, 2, 0.01) # need to watch a CFL-like condition
 
 delta = np.zeros_like(Xs)
-delta[500:-500] = 1 # this is a really interesting test case, we shouldn't be able to have a "temp" exceeding 1, yet we do
+delta[500:-500] = 3
 
 N_MAX = 6
-eqn = Galerkin('sines', inhomogeneous(Xs), Xs, N_MAX, times, c2=1, c0=1)
-func, times = eqn.runge_kutta()
+eqn = Galerkin('sines', inhomogeneous(Xs), Xs, N_MAX, times)
+eqn.runge_kutta(Xs)
+func, times = eqn.calc_answer()
+
+'''plt.plot(Xs, func[0])
+plt.show()'''
 
 plotAnimated(len(Xs), Xs, times, func, -1.25, 1.25)
+
+# 11/19: initial conditions are working now, still goofy though
+# Maybe look at some spectre stuff?
