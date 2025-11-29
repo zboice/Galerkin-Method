00a912bed839d23a4095a9a1ec2031dc6e89144b
Zachary Boice <65305552+zboice@users.noreply.github.com>
2025-11-28 21:56:11 -0500

    works for homogeneous chebyshev polynomials now

--- schrod.py
+++ schrod.py
@@ -18,7 +18,7 @@ class Galerkin:
             return np.ones_like(Xs)
         elif n==1:
             return Xs
-        return 2*Xs*Galerkin.cheb_T(n-1, Xs) - Galerkin.cheb_T(n-1, Xs)
+        return 2*Xs*Galerkin.cheb_T(n-1, Xs) - Galerkin.cheb_T(n-2, Xs)
 
     def cheb_U(n, Xs):
         if n==0:
@@ -35,18 +35,18 @@ class Galerkin:
             return np.zeros_like(Xs)
         return n*Galerkin.cheb_U(n-1, Xs)
 
+    #outlined on pg 29
     def homogeneous_cheb(n, a, Xs):
-        if n%2 == 0:
-            return Galerkin.cheb_T(n, Xs) - Galerkin.cheb_T(0, Xs)
-        return Galerkin.cheb_T(n, Xs) - Galerkin.cheb_T(1, Xs)
-    
+        return Galerkin.cheb_T(n, Xs) - Galerkin.cheb_T(n%2, Xs)
+        
     def homogeneous_cheb_deriv(n, a, Xs):
         if n%2 == 0:
             return n*Galerkin.cheb_U(n-1, Xs)
-        return n*Galerkin.cheb_U(n-1, Xs) - 1*Galerkin.cheb_U(0, Xs)
+        return n*Galerkin.cheb_U(n-1, Xs) - np.ones_like(Xs)
 
-    def basis_init(self, inhomogeneous, c0, c1, c2, c_non_lin):
+    def basis_init(self, inhomogeneous, off, c0, c1, c2, c_non_lin):
         if self.family == 'sines':
+            self.norm = 2/a
             self.nstart = 1
             self.bas = Galerkin.sines
             self.bas_der = Galerkin.sine_deriv
@@ -57,40 +57,47 @@ class Galerkin:
                                       + c_non_lin*self.bas(n, a, self.coord)**2*self.bas_der(m, a, self.coord))
             
         elif self.family == 'cheb':
+            self.norm = 1/np.sqrt(1-self.coord**2)
             self.nstart = 0
             self.bas = Galerkin.chebs
             self.bas_der = Galerkin.cheb_deriv
 
         elif self.family == 'homog_cheb':
+            self.norm = (2/np.pi)*1/np.sqrt(1-self.coord**2)
             self.nstart = 2
-            self.bas = Galerkin.homogeneous_cheb
-            self.bas_der = Galerkin.homogeneous_cheb_deriv
+            self.bas = lambda n, a, Xs: Galerkin.homogeneous_cheb(n, a, 2*Xs-off*np.ones_like(Xs))
+            self.bas_der = lambda n, a, Xs: 2*Galerkin.homogeneous_cheb_deriv(n, a, 2*Xs-off*np.ones_like(Xs))
 
-    def __init__(self, bas_fam, inhomogeneous, coordinate_axis, max_basis, times, c0=0, c1=0, c2=0, c_non_lin=0):
+    def __init__(self, bas_fam, inhomogeneous, coordinate_axis, max_basis, times, c0=0, c1=0, c2=0, c_non_lin=0, off=0):
         self.coord = coordinate_axis
         self.num = max_basis
         self.family = bas_fam
         self.times = times
-        self.basis_init(inhomogeneous, c0, c1, c2, c_non_lin)
+        self.basis_init(inhomogeneous, off, c0, c1, c2, c_non_lin)
 
     def derivs(self, t, c_vec, h, a, N):
         hbar = 1; m=1
         M_mat = np.zeros([N-self.nstart, N-self.nstart])
         L_mat = np.zeros_like(M_mat)
-        #F_vec = np.zeros(N-self.nstart)
 
         for n in range(self.nstart, N):
             for m in range(self.nstart, N):
                 M_mat[n-self.nstart, m-self.nstart] = simpson_int(self.bas(n, a, Xs)*self.bas(m, a, Xs), h)
                 L_mat[n-self.nstart, m-self.nstart] = simpson_int(self.bas_der(n, a, Xs)*self.bas_der(m, a, Xs), h)
 
-            #F_vec[n-self.nstart] = simpson_int( self.bas(n, a, self.coord)*self.inho(n, t) , h)
-
         return -1j*(hbar/2*m)*np.matmul(np.linalg.inv(M_mat), np.matmul(L_mat, c_vec))
 
-    def initial_cvec(self, answer, c0):
+    def initial_approx(self, h, Xs):        
+        answer = 0
         for n in range(self.nstart, self.num):
-            answer[n-self.nstart] = 2/a * simpson_int(self.bas(n, a, self.coord)*c0, h)
+            weight = simpson_int(self.bas(n, a, Xs)*initial_func(Xs) * self.norm , h)
+            answer += self.bas(n, a, Xs)*weight
+    
+        return answer
+
+    def initial_cvec(self, answer, u0):
+        for n in range(self.nstart, self.num):
+            answer[n-self.nstart] = simpson_int(self.bas(n, a, self.coord)*u0 * self.norm, h) # issue with norm (weighting function)
         return answer
 
     def runge_kutta(self, u0):
@@ -127,37 +134,52 @@ class Galerkin:
 def plotAnimated(Nx, x, t, func, ymin=-1, ymax=1):
     #plt.style.use('dark_background')
     fig = plt.figure()
-    axis = plt.axes(xlim=(x[0], x[-1]), ylim=(ymin, ymax)) #ylim=(1.1*np.min(func), 1.1*np.max(func)))
-    line1, = axis.plot([], [], lw = 3)
+    axis = plt.axes(xlim=(x[0], x[-1]), ylim=(ymin, ymax))
+    line, = axis.plot([], [], lw = 3)
 
-    ani = plt_ani.FuncAnimation(fig, animate, t, fargs=(line1, Nx, x, func), blit=True, interval=20)
+    ani = plt_ani.FuncAnimation(fig, animate, t, fargs=(line, Nx, x, func), blit=True, interval=20)
     plt.show()
 
 def init(line):
     line.set_data([], [])
     return line,
 
-def animate(t, line1, Nx, x, func):
-    line1.set_data(x, np.real(func[t][:2*Nx]))
-    return line1,
+def animate(t, line, Nx, x, func):
+    line.set_data(x, np.real(func[:][t][:2*Nx]))
+    return line,
 
 def initial_func(Xs):
-    return -(Xs - a/2 * np.ones_like(Xs))**2 #+ a/2*np.ones_like(Xs) #1/2 * np.sin(1*np.pi*Xs/a) + np.sqrt(3)/2 * np.sin(2*np.pi*Xs/a)
+    return -(Xs - a/2 * np.ones_like(Xs))**2 + (a/2)**2*np.ones_like(Xs) #1/2 * np.sin(1*np.pi*Xs/a) + np.sqrt(3)/2 * np.sin(2*np.pi*Xs/a)
+
+def compare_initial_info():
+    plt.plot(Xs, initial_func(Xs))
+
+    sine_test = Galerkin('sines', None, Xs, 5, None)
+    plt.plot(Xs, sine_test.initial_approx(h, Xs))
+
+    cheb_test = Galerkin('homog_cheb', None, Xs, 3, None, off=a)
+    plt.plot(Xs, cheb_test.initial_approx(h, Xs))
+    # the chebyshev series doesn't agree with the fourier series
+    plt.show()
+
+def do_animations():
+    times = np.arange(0, 1, ht) # need to watch a CFL-like condition
+    N_MAX = 5
+
+    if ht/h >= 1:
+        # sines, homog_cheb, cheb
+        eqn = Galerkin('sines', 1, Xs, N_MAX, times, off=a)
+        eqn.runge_kutta(initial_func(Xs))
+        func, times = eqn.calc_answer()
+        plotAnimated(len(Xs), Xs, times, func, -1, 1)
+
+    else:
+        print('rip bozo')
 
 a = 1
 k = 3
-h = 0.5e-3
-ht = 0.01
+h = 0.5e-2
+ht = 0.005
 Xs = np.arange(0, a, h)
-times = np.arange(0, 2, ht) # need to watch a CFL-like condition
-N_MAX = 4
 
-if ht/h > 1:
-    eqn = Galerkin('sines', 1, Xs, N_MAX, times)
-    eqn.runge_kutta(initial_func(Xs))
-    func, times = eqn.calc_answer()
-
-    plotAnimated(len(Xs), Xs, times, func, -1.25, 1.25)
-
-else:
-    print('rip bozo')
+do_animations()
